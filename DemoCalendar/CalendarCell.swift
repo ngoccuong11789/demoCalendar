bd67a1c9a5320c4b8d96838ba02a8ac44529fe13
spcv <spcv@spcvs-MacBook-Air.local>
2017-06-16 11:50:26 +0700

    first commit

---
+++ DemoCalendar/CalendarCell.swift
@@ -0,0 +1,21 @@
+//
+//  CalendarCell.swift
+//  DemoCalendar
+//
+//  Created by spcv on 6/15/17.
+//  Copyright © 2017 spcv. All rights reserved.
+//
+
+import UIKit
+
+class CalendarCell: JTAppleCell {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ DemoCalendar/CalendarCell2.swift
@@ -0,0 +1,21 @@
+//
+//  CalendarCell2.swift
+//  DemoCalendar
+//
+//  Created by spcv on 6/16/17.
+//  Copyright © 2017 spcv. All rights reserved.
+//
+
+import UIKit
+
+class CalendarCell2: JTAppleCell {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
